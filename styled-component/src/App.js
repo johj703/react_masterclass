9bf833574c3a068f10d7bde9bbb9d2ad818204f2
Jo <johj703@nate.com>
2023-09-18 23:05:16 +0900

    Update App.js

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -6,6 +6,10 @@ const Title = styled.h1`
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
