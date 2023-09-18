796f04f0e6ae1df51e98c285506f7d32aa9fb75b
Jo <johj703@nate.com>
2023-09-18 22:59:52 +0900

    Themes 세팅2

--- styled-component/src/index.js
+++ styled-component/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 
+const darkTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
