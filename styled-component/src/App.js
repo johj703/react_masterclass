edcb5826d382d769e4305ced31d20f2a3cabf0fc
Jo <johj703@nate.com>
2023-09-18 21:42:07 +0900

    'As' and Attrs 세팅

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -4,23 +4,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
 function App() {
-  return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </Father>
-  );
+  return <Father></Father>;
 }
 
 export default App;
