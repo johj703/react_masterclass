568db8b06744347ab033745f77382819022f6e70
Jo <johj703@nate.com>
2023-09-18 21:15:32 +0900

    styled-component를 사용하면 태그들이 깔끔해진다

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -4,11 +4,22 @@ const Father = styled.div`
   display: flex;
 `;
 
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
 function App() {
   return (
     <Father>
-      <div style={{ background: "teal", width: 100, height: 100 }}></div>
-      <div style={{ background: "tomato", width: 100, height: 100 }}></div>
+      <BoxOne />
+      <BoxTwo />
     </Father>
   );
 }
