a5113793176c32ac353c27fcd9ba944a5e34e603
Jo <johj703@nate.com>
2023-09-18 23:01:32 +0900

    Themes 세팅3

--- styled-component/src/index.js
+++ styled-component/src/index.js
@@ -8,6 +8,11 @@ const darkTheme = {
   backgroundColor: "#111",
 };
 
+const lightTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
