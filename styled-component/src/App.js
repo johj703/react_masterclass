3310fdf9ac153044a942ece2e45fabe794a15859
Jo <johj703@nate.com>
2023-09-18 21:03:53 +0900

    styled-component 사용방법

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
 
+const Father = styled.div`
+  display: flex;
+`;
+
 function App() {
   return (
-    <div style={{ display: "flex" }}>
+    <Father>
       <div style={{ background: "teal", width: 100, height: 100 }}></div>
       <div style={{ background: "tomato", width: 100, height: 100 }}></div>
-    </div>
+    </Father>
   );
 }
 
