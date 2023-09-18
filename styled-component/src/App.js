cdcee9ddfd7030a1ad13925a3a5a3a99eec8ef9d
Jo <johj703@nate.com>
2023-09-18 22:02:57 +0900

    회전하는 사각형 만들기

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -4,10 +4,20 @@ const Wrapper = styled.div`
   display: flex;
 `;
 
+const rotationAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
   background-color: tomato;
+  animation: ${rotationAnimation} 1s linear infinite;
 `;
 
 function App() {
