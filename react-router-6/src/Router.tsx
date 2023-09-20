79c24e4f1f15554a8c369f804ee5bb7e4da40bf0
Jo <johj703@nate.com>
2023-09-21 01:47:25 +0900

    Router 페이지 작성

--- react-router-6/src/Router.tsx
+++ react-router-6/src/Router.tsx
@@ -1,6 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import About from "./screens/About";
 
 function Router() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default Router;
