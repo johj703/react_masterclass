fb18aba419eb265ba92ab6e5c9e6b9911f72fbfc
Jo <johj703@nate.com>
2023-09-21 01:43:40 +0900

    Home, about 페이지 라우터 분리

--- react-router-6/src/components/Header.tsx
+++ react-router-6/src/components/Header.tsx
@@ -5,8 +5,12 @@ function Header() {
     <header>
       <ul>
         <Link to={"/"}>Home</Link>
+      </ul>
+      <ul>
         <Link to={"/about"}>About</Link>
       </ul>
     </header>
   );
 }
+
+export default Header;
