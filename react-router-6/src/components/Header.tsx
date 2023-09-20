44352b1d2be3453594f2ea350bec193a68bfcc59
Jo <johj703@nate.com>
2023-09-21 01:42:38 +0900

    Header 파일 수정

--- react-router-6/src/components/Header.tsx
+++ react-router-6/src/components/Header.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <h1>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-    </h1>
+    <header>
+      <ul>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/about"}>About</Link>
+      </ul>
+    </header>
   );
 }
