67cc07e9e27d0637ef2f7d53a256993415e3b5de
HyunJun-Jo <johj703@nate.com>
2023-09-22 23:52:46 +0900

    BrowserRouter를 Routes에 감싸기

--- crypto_tracker/src/Router.tsx
+++ crypto_tracker/src/Router.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <Routes>
-      <Route path="/:coinId" element={<Coin />}></Route>
-      <Route path="/" element={<Coins />}></Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:coinId" element={<Coin />}></Route>
+        <Route path="/" element={<Coins />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default Router;
