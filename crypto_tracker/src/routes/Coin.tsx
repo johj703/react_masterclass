084b9a9f2c47d7aae1355ad899292f6ae9bc5cd3
HyunJun-Jo <johj703@nate.com>
2023-09-22 23:52:20 +0900

    Coin 컴포넌트 수정

--- crypto_tracker/src/routes/Coin.tsx
+++ crypto_tracker/src/routes/Coin.tsx
@@ -1,8 +1,11 @@
 import { useParams } from "react-router";
 
+interface Params {
+  coinId: string;
+}
+
 function Coin() {
-  const {params} = useParams();
-  console.log(params)
-  return <h1>Coin</h1>
+  const { coinId } = useParams() as unknown as Params;
+  return <h1>Coin: {coinId}</h1>
 }
 export default Coin;
