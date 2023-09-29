753a1cf753eeefb88683c7021fe4d32654ecaacb
Jo <johj703@nate.com>
2023-09-29 22:21:34 +0900

    MyVariants 생성해서 Box 속성이었던 initial과 animate를 가지고 가도록 작성

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { motion } from 'framer-motion'
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,21 +7,26 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
   border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06); 
-`
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360 },
+};
 
 function App() {
   return (
-   <Wrapper>
-    <Box transition={{type: "spring", bounce: 0.1}} initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360}} />
-   </Wrapper>
+    <Wrapper>
+      <Box transition={{ type: "spring", bounce: 0.1 }} />
+    </Wrapper>
   );
 }
 
