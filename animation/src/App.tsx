6aa515aa3fe72a7c268fe62b23a3e2d8f3a25604
Jo <johj703@nate.com>
2023-09-30 17:49:23 +0900

    사각형의 둥근 모서리를 40px로 크게 수정

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -15,7 +15,7 @@ const Box = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 10px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
