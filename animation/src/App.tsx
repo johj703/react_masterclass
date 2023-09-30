582f075ef4f9dca0e6ee3ce175735615f70df436
Jo <johj703@nate.com>
2023-09-30 17:48:08 +0900

    사각형 안에 원이 위치하도록 위치 수정2

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -23,6 +23,7 @@ const Circle = styled(motion.div)`
   background-color: white;
   height: 70px;
   width: 70px;
+  place-self: center;
   border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
