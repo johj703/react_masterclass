dd0002830b25b4237cc52e033b6506d1c658e052
Jo <johj703@nate.com>
2023-09-30 17:47:36 +0900

    사각형 안에 원이 위치하도록 위치 수정

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -12,6 +12,8 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   background-color: rgba(255, 255, 255, 0.2);
   border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
