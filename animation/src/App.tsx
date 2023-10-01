3f5042483e4ccacbf471c0dc1d88c9da9509527c
Jo <johj703@nate.com>
2023-10-01 21:30:03 +0900

    Update App.tsx

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -28,10 +28,19 @@ const Circle = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const boxVariants = {
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
+  },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box>
+      <Box variants={boxVariants} initial="start" animate="end">
         <Circle />
         <Circle />
         <Circle />
