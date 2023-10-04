9de26b2e7963122333178bb1c5459a5e173b1cf0
Jo <johj703@nate.com>
2023-10-04 15:51:42 +0900

    drag를 x축으로만 이동이 가능함.

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -20,14 +20,14 @@ const Box = styled(motion.div)`
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(46, 204, 113)", transition: {duration: 10} },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 };
 
 function App() {
   return (
     <Wrapper>
       <Box
-        drag
+        drag="x"
         variants={boxVariants}
         whileHover="hover"
         whileDrag="drag"
