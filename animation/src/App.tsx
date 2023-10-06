57d49e2d69168b05246d431fbad9d2414de6c82a
HyunJun-Jo <johj703@nate.com>
2023-10-06 15:52:57 +0900

    사각형을 왼쪽으로 drag하면 하늘색으로, 오른쪽으로 drag하면 연두색으로 변하도록 작성

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -8,7 +7,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, rgb(238, 0, 153), #d0e);
+  background: ;
 `;
 
 const Box = styled(motion.div)`
@@ -22,9 +21,17 @@ const Box = styled(motion.div)`
 function App() {
   const x = useMotionValue(0);
   const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
- 
+  const gradient = useTransform(
+    x, 
+    [-800, 0, 800], 
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 238, 0))"
+  ]
+  );
   return (
-    <Wrapper>
+    <Wrapper style={{background: gradient}}>
       <Box style={{ x, rotateZ }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
