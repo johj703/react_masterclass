db7107761602f9904668d24b2182d250e6c151b7
HyunJun-Jo <johj703@nate.com>
2023-10-06 14:56:53 +0900

    useTransform을 사용해서 어느 하나의 인자 값을 받고, 인자 값의 제한 값과 원하는 출력 값을 받을 수 있도록 작성

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 const Wrapper = styled.div`
@@ -20,8 +20,10 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
+  const potato = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   useEffect(() => {
-    x.onChange(() => console.log(x.get()));
+   // x.onChange(() => console.log(x.get()));
+   potato.onChange(() => console.log(potato.get()));
   }, [x]);
   return (
     <Wrapper>
