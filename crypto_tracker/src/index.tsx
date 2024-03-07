478cddab2939615348b8df84a47e246376d2c7e8
Jo <johj703@nate.com>
2024-03-07 15:18:46 +0900

    Update index.tsx

--- crypto_tracker/src/index.tsx
+++ crypto_tracker/src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from "styled-components"
-import {theme} from "./theme"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
