a55563181bd764c9df6f57b283fe94394392b7dd
Jo <johj703@nate.com>
2024-03-08 14:50:22 +0900

    Container, Header, CoinList, Coin style 입힐 준비

--- crypto_tracker/src/routes/Coins.tsx
+++ crypto_tracker/src/routes/Coins.tsx
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li``;
+
 const Title = styled.div`
   color: ${(props) => props.theme.accentColor};
 `;
