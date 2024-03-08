5fa593e91782bac96874aba4fe2918b6d3ca0294
Jo <johj703@nate.com>
2024-03-08 14:56:57 +0900

    coins 배열 추가

--- crypto_tracker/src/routes/Coins.tsx
+++ crypto_tracker/src/routes/Coins.tsx
@@ -11,6 +11,36 @@ const Coin = styled.li``;
 const Title = styled.div`
   color: ${(props) => props.theme.accentColor};
 `;
+// https://api.coinpaprika.com/v1/coins
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
 
 function Coins() {
   return (
