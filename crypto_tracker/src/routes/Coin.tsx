58cfa9614389568dfc6096b83b6606d6455a6958
Jo <johj703@nate.com>
2024-03-08 17:01:23 +0900

    Coins에서 재활용한 코드로 Coin 페이지 작성

--- crypto_tracker/src/routes/Coin.tsx
+++ crypto_tracker/src/routes/Coin.tsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import { useParams } from "react-router";
+import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 interface RouteParams {
   coinId: string;
 }
 
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 export default Coin;
