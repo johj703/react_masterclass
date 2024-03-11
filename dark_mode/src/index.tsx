21845a33648029091395372997b0eb5f8dde8da2
Jo <johj703@nate.com>
2024-03-11 11:45:09 +0900

    index.tsx 수정

--- dark_mode/src/index.tsx
+++ dark_mode/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App";
 
 const queryClient = new QueryClient();
