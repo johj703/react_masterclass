13ad9aa43cf67aa6ea2363fd5bec31b9d863ee74
Jo <johj703@nate.com>
2024-03-12 23:17:08 +0900

    Update App.tsx

--- dark_mode/src/App.tsx
+++ dark_mode/src/App.tsx
@@ -1,5 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -70,7 +69,6 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <ToDoList />
     </>
   );
 }
