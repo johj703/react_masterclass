6f1d832be2fce6fe2e6e03db94effafee233176b
Jo <johj703@nate.com>
2024-03-12 21:23:34 +0900

    Router에 다크모드에 대한 interface 추가

--- dark_mode/src/Router.tsx
+++ dark_mode/src/Router.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
+interface IRouterProps {
+  toggleDark: () => void;
+}
+
 function Router() {
   return (
     <BrowserRouter>
