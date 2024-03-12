b5392ef78aed5f9840db968068094d2d68ac6f8d
Jo <johj703@nate.com>
2024-03-12 23:26:42 +0900

    value state 생성, onChange 함수 작성 및 input에 등록

--- dark_mode/src/ToDoList.tsx
+++ dark_mode/src/ToDoList.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
 function ToDoList() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
   return (
     <div>
       <form>
-        <input placeholder="Write a to do" />
+        <input onChange={onChange} value={value} placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
