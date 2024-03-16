d0a2f20146ac0d6da9c985988ade58762704bbce
Jo <johj703@nate.com>
2024-03-17 00:00:39 +0900

    boxVariants 에서 initial, visible, leaving에 각각 속성 추가

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -20,6 +20,22 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    y: 20,
+  },
+}
+
 function App() {
   const [showing, setShowing] = useState(false);
   const toggleShowing = () => setShowing((prev) => !prev);
