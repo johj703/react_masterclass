0111c6cad4cc7e5830bf1fdc60aa2f4e694e4b6d
Jo <johj703@nate.com>
2024-03-17 18:43:48 +0900

    Update App.tsx

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -20,32 +20,10 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    rotateZ: 360,
-  },
-  leaving: {
-    opacity: 0,
-    scale : 0,
-    y: 50,
-  },
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-      <AnimatePresence>
-        {showing ? <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving" /> : null}
-      </AnimatePresence>
+      <Box></Box>
     </Wrapper>
   );
 }
