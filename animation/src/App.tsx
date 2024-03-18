e2d9a4e0751e2b7e6463fe01ba63b82895e1f982
Jo <johj703@nate.com>
2024-03-18 19:42:04 +0900

    Update App.tsx

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -12,15 +12,12 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
