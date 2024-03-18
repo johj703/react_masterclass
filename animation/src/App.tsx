734147be8787e1609befd0baa0d8224f15f88b09
Jo <johj703@nate.com>
2024-03-18 19:39:09 +0900

    mode="wait" 삭제

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -62,7 +62,7 @@ function App() {
   };
   return (
     <Wrapper>
-      <AnimatePresence mode="wait" custom={back}>
+      <AnimatePresence custom={back}>
         <Box
           custom={back}
           variants={box}
