51619b4e8debcbe3a88fa47b683bc008332d6f48
Jo <johj703@nate.com>
2024-03-19 22:21:27 +0900

    덮어지는 검은 화면이 조금 더 어두워지도록 작성

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -39,7 +39,7 @@ const Circle = styled(motion.div)`
 const Overlay = styled(motion.div)`
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.7);
   position: absolute;
 `;
 
