030445c635c6b522750911882ea239f0697174d2
Jo <johj703@nate.com>
2024-03-19 18:49:46 +0900

    여러가지 사각형 중에서 하나를 클릭했을 때, 가운데로 이동하면서 커지는 animation 세팅

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -14,16 +14,18 @@ const Wrapper = styled(motion.div)`
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  height: 200px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
