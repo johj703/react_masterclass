ce101ef4a3205c46e6d28e9eb8af16b1fdc9fda9
Jo <johj703@nate.com>
2024-03-20 11:16:04 +0900

    Nav, Logo, Items, Item에 style 추가

--- nomflix/src/Components/Header.tsx
+++ nomflix/src/Components/Header.tsx
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Col = styled.div``;
 
-const Logo = styled.svg``;
+const Logo = styled.svg`
+  margin-right: 50px;
+`;
 
-const Items = styled.ul``;
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
 
-const Item = styled.li``;
+const Item = styled.li`
+  margin-right: 20px;
+`;
 
 function Header() {
   return (
