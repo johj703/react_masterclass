337e13e615f710d9d0b25757b9994a8ee95dba9b
Jo <johj703@nate.com>
2024-03-20 23:16:23 +0900

    motion.svg로 변경

--- nomflix/src/Components/Header.tsx
+++ nomflix/src/Components/Header.tsx
@@ -122,7 +122,7 @@ function Header() {
       </Col>
       <Col>
         <Search onClick={openSearch}>
-          <svg
+          <motion.svg
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -132,7 +132,7 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </motion.svg>
           <Input
             animate={{ scaleX: searchOpen ? 1 : 0 }}
             placeholder="Search for movie or tv show!"
