07e0a56b04f65e5350d7cc3651dbf2313b0c55cc
Jo <johj703@nate.com>
2024-03-20 11:11:16 +0900

    styled-components를 사용해서 만들어야 할 기초 부분 작성하기

--- nomflix/src/Components/Header.tsx
+++ nomflix/src/Components/Header.tsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
+const Nav = styled.nav``;
+
+const Col = styled.div``;
+
+const Logo = styled.svg``;
+
 function Header() {
-  return null;
+  return (
+    <Nav>
+      <Col>
+        <Logo />
+        <Items>
+          <Item>Home</Item>
+          <Item>Tv Shows</Item>
+        </Items>
+      </Col>
+      <Col>
+        <button>search</button>
+      </Col>
+    </Nav>
+  );
 }
 
 export default Header;
