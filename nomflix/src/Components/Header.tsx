1066377eb4778b902fb459fc371dfed8a018b4d7
Jo <johj703@nate.com>
2024-03-22 01:39:17 +0900

    navVariants를 만들어서 중복 코드 줄이기

--- nomflix/src/Components/Header.tsx
+++ nomflix/src/Components/Header.tsx
@@ -92,6 +92,15 @@ const logoVariants = {
   },
 };
 
+const navVariants = {
+  top: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+  },
+  scroll: {
+    backgroundColor: "rgba(0, 0, 0, 1)",
+  },
+};
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
@@ -114,21 +123,14 @@ function Header() {
   useEffect(() => {
     scrollY.onChange(() => {
       if (scrollY.get() > 80) {
-        navAnimation.start({
-          backgroundColor: "rgba(0, 0, 0, 1)",
-        });
+        navAnimation.start("scroll");
       } else {
-        navAnimation.start({
-          backgroundColor: "rgba(0, 0, 0, 0)",
-        });
+        navAnimation.start("top");
       }
     });
-  }, [scrollY]);
+  }, [scrollY, navAnimation]);
   return (
-    <Nav
-      animate={navAnimation}
-      initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-    >
+    <Nav variants={navVariants} animate={navAnimation} initial={"top"}>
       <Col>
         <Logo
           variants={logoVariants}
