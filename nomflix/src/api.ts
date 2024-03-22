334c1d180719a98581c2653a6014b25b5fd55311
Jo <johj703@nate.com>
2024-03-22 18:42:52 +0900

    base_path와 영화를 가지고 오는 getMovies fetch 함수 작성

--- nomflix/src/api.ts
+++ nomflix/src/api.ts
@@ -1,1 +1,8 @@
 const API_KEY = "96af03deebf6400a12a9c9ce252f33b5";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}//movie/now_playing?api-key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
