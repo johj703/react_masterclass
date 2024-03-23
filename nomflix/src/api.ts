bbb94618e76472a2d9a39f537c198ecac03ea1bb
Jo <johj703@nate.com>
2024-03-23 12:44:54 +0900

    IMovie 인터페이스 생성 후 results에 붙이기

--- nomflix/src/api.ts
+++ nomflix/src/api.ts
@@ -1,13 +1,20 @@
 const API_KEY = "96af03deebf6400a12a9c9ce252f33b5";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
 export interface IGetMovieResult {
   dates: {
     maximum: string;
     minimum: string;
   };
   page: number;
-  results: [];
+  results: IMovie[];
   total_pages: number;
   total_results: number;
 }
