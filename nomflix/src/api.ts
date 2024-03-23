efb089710013f4b7609c3605ae880ab1b51089b7
Jo <johj703@nate.com>
2024-03-23 12:12:47 +0900

    interface 추가

--- nomflix/src/api.ts
+++ nomflix/src/api.ts
@@ -1,6 +1,17 @@
 const API_KEY = "96af03deebf6400a12a9c9ce252f33b5";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+export interface IGetMovieResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: [];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}//movie/now_playing?api-key=${API_KEY}`).then(
     (response) => response.json()
