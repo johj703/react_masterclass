05650c4b6296dab3187e6551346a9314a91b4c90
Jo <johj703@nate.com>
2024-03-23 17:42:10 +0900

    배경화면 상단은 어둡게 만들고 아래는 밝게 만드는 효과 작성

--- nomflix/src/Routes/Home.tsx
+++ nomflix/src/Routes/Home.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { IGetMovieResult, getMovies } from "../api";
 import styled from "styled-components";
+import { makeImagePath } from "../utils";
 
 const Wrapper = styled.div`
   background: black;
@@ -13,12 +14,15 @@ const Loader = styled.div`
   align-items: center;
 `;
 
-const Banner = styled.div`
+const Banner = styled.div<{ bgPhoto: string }>`
   height: 10vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
 `;
 
 const Title = styled.h2`
@@ -43,7 +47,7 @@ function Home() {
         <Loader>Loading</Loader>
       ) : (
         <>
-          <Banner>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
