57cad4f4a14ef2a7bed571824558dceb2658de9c
Jo <johj703@nate.com>
2024-03-24 23:31:36 +0900

    영화 box에 커서를 올리면 영화가 올라가면서 제목이 나타나도록 작성

--- nomflix/src/Routes/Home.tsx
+++ nomflix/src/Routes/Home.tsx
@@ -57,6 +57,7 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-position: center center;
   height: 200px;
   font-size: 66px;
+  position: relative;
   &:first-child {
     transform-origin: center left;
   }
@@ -66,9 +67,16 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
 `;
 
 const Info = styled(motion.div)`
-  padding: 20px;
+  padding: 10px;
   background-color: ${(props) => props.theme.black.lighter};
   opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
 `;
 
 const rowVariants = {
@@ -88,6 +96,7 @@ const boxVariants = {
     scale: 1,
   },
   hover: {
+    zIndex: 99,
     scale: 1.3,
     y: -50,
     transition: {
@@ -101,6 +110,11 @@ const boxVariants = {
 const infoVariants = {
   hover: {
     opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    },
   },
 };
 
@@ -158,7 +172,9 @@ function Home() {
                       transition={{ type: "tween" }}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
                     >
-                      <Info variants={infoVariants} />
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
                     </Box>
                   ))}
               </Row>
