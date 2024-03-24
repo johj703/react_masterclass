65b2574b4cfaa7ce2f3e9751fe752a0834f01017
Jo <johj703@nate.com>
2024-03-24 23:56:33 +0900

    영화 box를 클릭하면 주소창에 해당 영화의 movieId가 추가되도록 작성2

--- nomflix/src/App.tsx
+++ nomflix/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="movies/:movieId" element={<Home />} />
         <Route path="/tv" element={<Tv />} />
         <Route path="/search" element={<Search />} />
       </Routes>
